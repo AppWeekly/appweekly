58adb354572b10f898c7a77cd35ec4cf5810f5f3
Roman Velic <roman.velic@gmail.com>
2023-05-28 22:50:38 +0200

    Craft prompt, generate image via DALLE2, generate audio, combine to video

---
+++ pages/api/synthesize.js
@@ -0,0 +1,172 @@
+import ffmpeg from 'fluent-ffmpeg';
+import tempfile from 'tempfile';
+import { Configuration, OpenAIApi } from 'openai';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import path from 'path';
+import os from 'os';
+import fs from 'fs';
+
+const { 
+  NEXT_PUBLIC_OPENAI_API_KEY,
+  NEXT_PUBLIC_PLAYHT_USER_ID,
+  NEXT_PUBLIC_PLAYHT_SECRET_KEY
+} = process.env
+
+const openai = new OpenAIApi(new Configuration({
+  apiKey: NEXT_PUBLIC_OPENAI_API_KEY,
+}));
+
+// Support for local (mac) & production (linux)
+let ffmpegPath;
+if (os.type() === 'Linux') {
+  ffmpegPath = path.join(process.cwd(), 'bin', 'ffmpeg');
+} else if (os.type() === 'Darwin') {
+  ffmpegPath = 'ffmpeg'; // This assumes FFmpeg is installed via Homebrew and available in the system PATH
+} else {
+  throw new Error('Unsupported platform');
+}
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+async function getImagePrompt(text) {
+  const prompt = `Given the following text, create a succinct summary that could be used as description of a popular painting: ${text}`
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: prompt}],
+    max_tokens: 100
+  });
+
+  if (response.status !== 200) {
+    throw new Error(`Failed to generate prompt, status code: ${response.status}`);
+  }
+
+  return response.data.choices[0].message.content;
+}
+
+async function getImage(prompt) {
+  const response = await openai.createImage({
+    prompt: prompt,
+    n: 1,
+    size: "256x256",
+  });
+
+  if (response.status !== 200) {
+    throw new Error(`Failed to generate image, status code: ${response.status}`);
+  }
+
+  return response.data.data[0];
+}
+
+async function getAudio(text) {
+  const headers = {
+    'accept': 'text/event-stream',
+    'content-type': 'application/json',
+    'AUTHORIZATION': `Bearer ${NEXT_PUBLIC_PLAYHT_SECRET_KEY}`,
+    'X-USER-ID': NEXT_PUBLIC_PLAYHT_USER_ID
+  }
+  const response = await fetch('https://play.ht/api/v2/tts', {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      quality: 'medium',
+      output_format: 'mp3',
+      speed: 1,
+      sample_rate: 24000,
+      text: text,
+      voice: 'larry'
+    })
+  });
+
+  return new Promise((resolve, reject) => {
+    response.body.on('error', reject);
+    response.body.on('data', async (data) => {
+      const strData = data.toString('utf8');
+      try {
+        if (strData.indexOf('event: completed') > -1) {
+          const jsonMatch = strData.match(/{.*}/);
+          resolve(JSON.parse(jsonMatch[0]));
+        }
+      } catch (err) {
+        reject(err)
+      }
+    })
+  })
+}
+
+async function downloadFile(url) {
+  const response = await axios.get(url, {
+    responseType: 'stream'
+  });
+
+  if (response.status !== 200) {
+    throw new Error(`Failed to download file, status code: ${response.status}`);
+  }
+
+  let tempFilePath;
+  if (response.headers['content-type'].includes('audio')) {
+    tempFilePath = tempfile({ extension: '.mp3' });
+  } else if (response.headers['content-type'].includes('image')) {
+    tempFilePath = tempfile({ extension: '.png' });
+  } else {
+    throw new Error('Unsupported file type');
+  }
+
+  const fileStream = fs.createWriteStream(tempFilePath);
+  response.data.pipe(fileStream);
+
+  return new Promise((resolve, reject) => {
+    fileStream.on('finish', () => resolve(tempFilePath));
+    fileStream.on('error', reject);
+  });
+}
+
+async function processVideo(imagePath, audioPath) {
+  const outputPath = tempfile({ extension: '.mp4' });
+  const response = await new Promise((resolve, reject) => {
+    ffmpeg()
+      .input(imagePath) // Path to the image file
+      .inputOptions('-loop 1') // Loop the image
+      .input(audioPath) // Path to the audio file
+      .outputOptions('-shortest') // Finish encoding when the shortest input stream ends
+      .outputOptions('-c:v libx264') // Codec video: libx264
+      .outputOptions('-tune stillimage') // Tune for still image input
+      .outputOptions('-c:a aac') // Codec audio: aac
+      .outputOptions('-b:a 192k') // Audio bitrate
+      .outputOptions('-pix_fmt yuv420p') // Pixel format
+      .output(outputPath) // Output file
+      .on('end', async () => resolve(outputPath))
+      .on('error', (err) => reject(err))
+      .run();
+  });
+
+  return response;
+}
+
+export default async function handler(req, res) {
+  const { text } = req.body;
+
+  if (req.method !== 'POST') {
+    return res.status(400).json({ 'error': 'Bad request' });
+  }
+
+  try {
+    const audioOutput = await getAudio(text);
+    // console.log('audioOutput', audioOutput)
+    const audioFilePath = await downloadFile(audioOutput.url);
+    // console.log('audioFilePath', audioFilePath)
+    const imagePrompt = await getImagePrompt(text);
+    // console.log('imagePrompt', imagePrompt)
+    const imageOutput = await getImage(imagePrompt);
+    // console.log('imageOutput', imageOutput)
+    const imageFilePath = await downloadFile(imageOutput.url);
+    // console.log('imageFilePath', imageFilePath)
+    const videoFilePath = await processVideo(imageFilePath, audioFilePath);
+    // console.log('videoFilePath', videoFilePath)
+
+    return res.status(200).json({ url: videoFilePath });
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json({ 'error': 'An error occurred. Please try again.' });
+  }
+}
